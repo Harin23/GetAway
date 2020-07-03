db635c3ad4a0bc687a39a89725f367c9c8dcf508
Harin23 <harin.s.bal@gmail.com>
2020-07-03 00:35:20 -0600

    backend linked to a database in sql

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,61 @@ var position = {
     y:100
 };
 
+//-------------------------------------------------------------------------sql server----------------------------------------------------------------------------------------------------------
+var sqlAuth= []; //update me
+
+var Connection = require('tedious').Connection;  
+    var config = {  
+        server: '127.0.0.1',  
+        authentication: {
+            type: 'default',
+            options: {
+                userName: sqlAuth[0], 
+                password: sqlAuth[1]  
+            }
+        },
+        options: {
+            // If you are on Microsoft Azure, you need encryption:
+            encrypt: false,
+            database: 'getaway'  //update me
+        }
+    }; 
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) { 
+        if (err) {console.log(err)}; 
+        // If no error, then good to proceed.  
+        console.log("Connected");  
+        executeStatement();  
+    });  
+  
+    var Request = require('tedious').Request;  
+    var TYPES = require('tedious').TYPES;  
+  
+    function executeStatement() {  
+        request = new Request("SELECT * FROM users;", function(err) {  
+        if (err) { console.log(err)};  
+        });  
+        var result = "";  
+        request.on('row', function(columns) {  
+            columns.forEach(function(column) {  
+              if (column.value === null) {  
+                console.log('NULL');  
+              } else {  
+                result+= column.value + " ";  
+              }  
+            });  
+            console.log(result);  
+            result ="";  
+        });  
+  
+        request.on('done', function(rowCount, more) {  
+        console.log(rowCount + ' rows returned');  
+        });  
+        connection.execSql(request);  
+    }  
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 socketio.on("connection", socket => {
     socket.emit("position", position);
     socket.on("move", data =>{
