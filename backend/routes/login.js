718cc005e9eb6612635f80e8ae7926511a44f4ca
Harin23 <harin.s.bal@gmail.com>
2020-07-31 15:02:44 -0600

    can login using username or email now/ front end redesigned

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -18,21 +18,35 @@ mongoose.connect(db_uri, error => {
 //this is the /login route:
 router.post('/', (req,res) => {
         let userinfo = req.body
-        
-        user_model.findOne({email: userinfo.email}, (err, user) => {
-            if (err){
-                console.log(err)
-            } else if (!user){
-                res.status(401).send('Invalid Email')
-            } else if (user.password != user.password){
-                res.status(401).send('Invalid Password')
-            } else {
-                let payload = { subject: user._id }
-                let token = jwt.sign(payload, 'secretkey')
-                res.status(200).send(token)
-            }
-        });
-        
+        if (userinfo.userid.indexOf('@') > -1){
+            userModel.findOne({ email: userinfo.userid }, (err, user) => {
+                if (err){
+                    console.log(err)
+                } else if (user == false){
+                    res.status(401).send('Invalid Email')
+                } else if (user.password != userinfo.password){
+                    res.status(401).send('Invalid Password')
+                } else {
+                    let payload = { subject: user._id }
+                    let token = jwt.sign(payload, 'secretkey')
+                    res.status(200).send(token)
+                }
+            });
+        }else{
+            userModel.findOne({ username: userinfo.userid }, (err, user) => {
+                if (err){
+                    console.log(err)
+                } else if (user == false){
+                    res.status(401).send('Invalid Username')
+                } else if (user.password != userinfo.password){
+                    res.status(401).send('Invalid Password')
+                } else {
+                    let payload = { subject: user._id }
+                    let token = jwt.sign(payload, 'secretkey')
+                    res.status(200).send(token)
+                }
+            });
+        }
     });
 
 //this is the /login/resgister route:
