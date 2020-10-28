af30023233d4ae007d96eaad98f5a40ccacd94b2
Tejpreet Bal <harin.s.bal@gmail.com>
2020-10-28 15:56:10 -0600

    gameview (In progress)

--- src/app/gameview/gameview.component.ts
+++ src/app/gameview/gameview.component.ts
@@ -1,23 +1,48 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-gameview',
   templateUrl: './gameview.component.html',
   styleUrls: ['./gameview.component.css']
 })
-export class GameviewComponent implements OnInit {
+export class GameviewComponent implements OnInit, AfterViewInit {
 
+  canvasHeight = window.innerHeight - 74;
+  canvasWidth = window.innerWidth*0.992;
 
-
+  
 
   constructor() { }
 
   ngOnInit() {
-    
+    //console.log(window.innerHeight - 74)
+    //console.log(window.innerWidth*0.992)
   }
 
-  move(direction: string){
-    console.log("move: " + direction)
-  };
+  ngAfterViewInit(){
+    let gamecanvas = document.getElementById('gamecanvas') as  HTMLCanvasElement;
+    let gcCTX = gamecanvas.getContext("2d");
 
+    //rectange of the table
+    let wr = this.canvasWidth*0.5;
+    let hr = this.canvasHeight*0.6;
+    let xr = (this.canvasWidth*0.5) - (0.5*wr);
+    let yr = (this.canvasHeight*0.5) - (0.5*hr);
+    gcCTX.fillRect(xr,yr,wr,hr);
+
+    //semi-circles of table
+    let r = hr*0.5;
+    let yc = yr + (0.5*hr);
+    let xc1 = xr;
+    gcCTX.arc(xc1,yc,r,0.5*Math.PI,1.5*Math.PI);
+    gcCTX.fillStyle = "RGB(53,101,77)"
+    gcCTX.fill();
+    let xc2 = xr + wr;
+    gcCTX.arc(xc2,yc,r,1.5*Math.PI,0.5*Math.PI);
+    gcCTX.fill();
+
+    //card on the table
+    
+
+  }
 }
