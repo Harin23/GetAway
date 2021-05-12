f250f3ab5ed574c1a787fb93e57e5b93fd12b247
Harin Bal <63739311+Harin23@users.noreply.github.com>
2021-05-12 17:12:13 -0600

    Update login.js

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -6,7 +6,7 @@ const userModel = require('../models/user');
 const middleware = require("./middleware")
 
 //connect to db
-db_uri = "mongodb+srv://harin_getaway_game24:vWey6Oa4D9wOzDY7@getaway.svfza.mongodb.net/getaway-users?retryWrites=true&w=majority"
+db_uri = process.env.DB
 mongoose.connect(db_uri, {useNewUrlParser: true, useUnifiedTopology: true }, error => {
     if (error){
         console.log(error)
